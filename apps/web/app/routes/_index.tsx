2f4c4b6feca2e17d88f2b1255d3ed1614dec8f70
coji <coji@techtalk.jp>
2024-05-12 21:27:41 +0900

    chore: Add project files and configurations for Vite and Remix

---
+++ apps/web/app/routes/_index.tsx
@@ -0,0 +1,41 @@
+import type { MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
+}
+
+export default function Index() {
+  return (
+    <div className="m-4 leading-8">
+      <h1 className="mb-4 text-4xl font-bold">Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
