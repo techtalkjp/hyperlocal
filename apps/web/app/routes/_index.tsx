d54e94b013a66625157e50cdca650e14141fd16f
coji <coji@techtalk.jp>
2024-08-11 15:53:56 +0900

    fix: tsconfig

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -10,32 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="m-4 leading-8">
-      <h1 className="mb-4 text-4xl font-bold">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1 className="mb-4 text-4xl font-bold">Hyperlocal Web</h1>
     </div>
   )
 }
