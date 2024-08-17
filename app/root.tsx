275a10a0464e9dec1a41f1b88d9efabd70b1f5ed
coji <coji@techtalk.jp>
2024-08-17 10:58:16 +0900

    feat: add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
 import globalStyles from './styles/globals.css?url'
 
@@ -19,10 +20,18 @@ export const meta: MetaFunction = () => [
 export const links = () => [{ rel: 'stylesheet', href: globalStyles }]
 
 export const loader = (args: LoaderFunctionArgs) => {
-  return rootAuthLoader(args)
+  return rootAuthLoader(args, () => {
+    const env = {
+      GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+      NODE_ENV: process.env.NODE_ENV,
+    }
+    return { env }
+  })
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { env } = useLoaderData<typeof loader>()
+
   return (
     <html lang="en">
       <head>
@@ -32,6 +41,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        {env.NODE_ENV === 'production' && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${env.GA_TRACKING_ID}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${env.GA_TRACKING_ID}');
+              `,
+              }}
+            />
+          </>
+        )}
+
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/services/env.server.ts
+++ app/services/env.server.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   GOOGLE_MAPS_API_KEY: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
+  GA_TRACKING_ID: z.string(),
 })
 
 envSchema.parse(process.env)
