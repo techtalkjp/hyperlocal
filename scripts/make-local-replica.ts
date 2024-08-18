68d880552c9fd03fe9290e467d25c98c6d9ddd35
coji <coji@techtalk.jp>
2024-08-18 19:07:47 +0900

    fix: remove unused sync interval

--- scripts/make-local-replica.ts
+++ scripts/make-local-replica.ts
@@ -7,7 +7,6 @@ const client = createClient({
   url: 'file:./data/dev.db',
   authToken: process.env.TURSO_AUTH_TOKEN,
   syncUrl: process.env.TURSO_DATABASE_URL,
-  syncInterval: 60,
 })
 
 await client.sync()
