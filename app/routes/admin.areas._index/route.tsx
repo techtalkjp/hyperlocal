a67d68476a7807e3df1177c81b3242fbaac33da2
coji <coji@techtalk.jp>
2024-08-26 09:32:41 +0900

    refactor: Update admin areas index table headers

--- app/routes/admin.areas._index/route.tsx
+++ app/routes/admin.areas._index/route.tsx
@@ -30,21 +30,23 @@ export default function AdminAreasIndexPage() {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>ID</TableHead>
+              <TableHead>Area ID</TableHead>
+              <TableHead>City ID</TableHead>
               <TableHead>Name</TableHead>
               <TableHead>Longitude</TableHead>
               <TableHead>Latitude</TableHead>
-              <TableHead>Updated At</TableHead>
-              <TableHead>Created At</TableHead>
+              <TableHead>Radius</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {areas.map((area) => (
               <TableRow key={area.areaId}>
                 <TableCell>{area.areaId}</TableCell>
+                <TableCell>{area.cityId}</TableCell>
                 <TableCell>{area.name}</TableCell>
                 <TableCell>{area.longitude}</TableCell>
                 <TableCell>{area.latitude}</TableCell>
+                <TableCell>{area.radius}</TableCell>
               </TableRow>
             ))}
           </TableBody>
