7172f38ae450db7126770b81b95f0cf8bb634ce5
coji <coji@techtalk.jp>
2024-08-30 09:32:24 +0900

    refactor: Update business status badge colors

--- app/features/place/components/business-status-badge.tsx
+++ app/features/place/components/business-status-badge.tsx
@@ -10,10 +10,10 @@ export const BusinessStatusBadge = ({ status }: { status: BusinessStatus }) => {
       <span className="text-orange-500">Closing Soon</span>
     ))
     .with(BusinessStatus.CLOSED, () => (
-      <span className="text-orange-500">Closed</span>
+      <span className="text-red-500">Closed</span>
     ))
     .with(BusinessStatus.CLOSED_OPENING_SOON, () => (
-      <span className="text-green-500">Opening Soon</span>
+      <span className="text-orange-500">Opening Soon</span>
     ))
     .with(BusinessStatus.UNKNOWN, () => <span />)
     .exhaustive()
