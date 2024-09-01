edd0dc224ce31368bc2d1ed39d5c3adb7c5b0675
coji <coji@techtalk.jp>
2024-09-01 21:31:58 +0900

    refactor: Update SEO route to include sitemap and robots.txt

--- app/routes/_seo.robots[.]txt/route.tsx
+++ app/routes/_seo.robots[.]txt/route.tsx
@@ -1,5 +1,4 @@
 import type { HeadersFunction, LoaderFunctionArgs } from '@remix-run/node'
-import cities from '~/consts/cities'
 
 export const headers: HeadersFunction = () => ({
   'Content-Type': 'application/xml;charset=utf-8',
@@ -8,21 +7,17 @@ export const headers: HeadersFunction = () => ({
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url)
-  const host = url.host.split('.')[0]
+  const origin = url.origin
 
-  const city = cities.find((city) => city.cityId === host) ?? cities[0]
+  const content = `# *
+User-agent: *
+Disallow: /admin
 
-  const content = `<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>https://hogosuru.dailove.com/sitemap-0.xml</loc>
-  </sitemap>
-  <sitemap>
-    <loc>https://hogosuru.dailove.com/sitemap/chemicals.xml</loc>
-  </sitemap>
-  <sitemap>
-    <loc>https://hogosuru.dailove.com/sitemap/news.xml</loc>
-  </sitemap>
-</sitemapindex>`
+# Host
+Host: ${origin}
+
+# Sitemaps
+Sitemap: ${origin}/sitemap.xml`
 
   return new Response(content)
 }
