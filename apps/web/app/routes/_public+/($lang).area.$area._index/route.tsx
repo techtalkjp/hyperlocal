1466b68454961a116f4ecdd9efd6a9339642067e
coji <coji@techtalk.jp>
2024-10-28 21:05:38 +0900

    Refactor category navigation in area route: Update component structure and styling

--- apps/web/app/routes/_public+/($lang).area.$area._index/route.tsx
+++ apps/web/app/routes/_public+/($lang).area.$area._index/route.tsx
@@ -1,13 +1,7 @@
 import { categories } from '@hyperlocal/consts'
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '~/components/ui'
+import { Card, CardHeader, CardTitle, Stack } from '~/components/ui'
 import { getPathParams } from '~/features/city-area/utils'
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
@@ -21,24 +15,25 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 export default function AreaIndexPage() {
   const { lang, area } = useLoaderData<typeof loader>()
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{area.i18n[lang.id]}</CardTitle>
-        <CardDescription>{area.description[lang.id]}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid grid-cols-2 gap-2">
-          {categories.map((category) => (
-            <Link to={`${category.id}`} key={category.id}>
-              <Card className="hover:bg-secondary">
-                <CardHeader>
-                  <CardTitle>{category.i18n[lang.id]}</CardTitle>
-                </CardHeader>
-              </Card>
-            </Link>
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+    <Stack>
+      <h3 className="text-xl font-semibold leading-none tracking-tight">
+        {area.i18n[lang.id]}
+      </h3>
+      <p className="text-sm text-muted-foreground">
+        {area.description[lang.id]}
+      </p>
+
+      <div className="grid grid-cols-2 gap-2">
+        {categories.map((category) => (
+          <Link to={`${category.id}`} key={category.id}>
+            <Card className="hover:bg-secondary">
+              <CardHeader>
+                <CardTitle>{category.i18n[lang.id]}</CardTitle>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </Stack>
   )
 }
