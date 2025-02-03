9b2d29eea4b1b31d8814ba96fb869f6b0c9c3d28
coji <coji@techtalk.jp>
2025-02-03 16:12:41 +0900

    feat: add script to create local database replica and improve error handling

--- apps/web/scripts/make-local-replica.ts
+++ apps/web/scripts/make-local-replica.ts
@@ -3,14 +3,17 @@ import dotenv from 'dotenv'
 import fs from 'node:fs/promises'
 
 dotenv.config()
-
-const url = new URL(process.env.DATABASE_URL)
-await fs.unlink(url.pathname)
-
-const client = createClient({
+const config = {
   url: process.env.DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
   syncUrl: process.env.TURSO_DATABASE_URL,
-})
+}
 
-await client.sync()
+const url = new URL(config.url)
+await fs.rm(url.pathname, { force: true })
+await fs.rm(`${url.pathname}-client_wal_index`, { force: true })
+
+console.log({ url: config.url, syncUrl: config.syncUrl })
+const client = createClient(config)
+const ret = await client.sync()
+console.log(ret)
