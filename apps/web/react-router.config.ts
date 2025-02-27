55ee1d22461ac59f136e76249d0c56dc53111659
coji <coji@techtalk.jp>
2025-02-27 13:57:31 +0900

    fix: add prerender route for /test in react-router configuration

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -4,6 +4,7 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   ssr: true,
+  prerender: ['/test'],
   // prerender: async () => {
   //   const routes = ['/robots.txt']
   //   // languages
