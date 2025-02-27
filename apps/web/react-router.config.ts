c0dee56ad045442f1d326e87159ecbc003cdb96e
coji <coji@techtalk.jp>
2025-02-27 17:14:23 +0900

    fix: refactor prerender function in react-router config to improve route generation

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,43 +1,31 @@
-// import { areas, categories, languages } from '@hyperlocal/consts'
-// import { db } from '@hyperlocal/db'
+import { areas, categories, languages } from '@hyperlocal/consts'
 import type { Config } from '@react-router/dev/config'
 
 export default {
   ssr: true,
-  prerender: ['/test'],
-  // prerender: async () => {
-  //   const routes = ['/robots.txt']
-  //   // languages
-  //   for (const lang of languages) {
-  //     const langPath = lang.id === 'en' ? '/' : `/${lang.id}`
-  //     routes.push(langPath)
+  // prerender: ['/test'],
+  prerender: () => {
+    const routes = ['/test']
+    // languages
+    for (const lang of languages) {
+      const langPath = lang.id === 'en' ? '/' : `/${lang.id}`
+      routes.push(langPath)
 
-  //     // area
-  //     for (const area of areas) {
-  //       const areaPath = `${lang.id === 'en' ? '/' : `/${lang.id}/`}area/${area.areaId}`
-  //       routes.push(areaPath)
+      // area
+      for (const area of areas) {
+        const areaPath = `${lang.id === 'en' ? '/' : `/${lang.id}/`}area/${area.areaId}`
+        routes.push(areaPath)
 
-  //       // sitemap
-  //       const sitemapPath = `/sitemap/${lang.id}.xml`
-  //       routes.push(sitemapPath)
-
-  //       // category
-  //       for (const category of categories) {
-  //         // ranking
-  //         for (const rankType of ['rating', 'review', 'nearme']) {
-  //           const rankPath = `${lang.id === 'en' ? '/' : `/${lang.id}/`}area/${area.areaId}/${category.id}/${rankType}`
-  //           routes.push(rankPath)
-  //         }
-  //       }
-  //     }
-
-  //     // place
-  //     const places = await db.selectFrom('places').select('id').execute()
-  //     for (const place of places) {
-  //       const placePath = `${lang.id === 'en' ? '/' : `/${lang.id}/`}place/${place.id}`
-  //       routes.push(placePath)
-  //     }
-  //   }
-  //   return routes
-  // },
+        // category
+        for (const category of categories) {
+          // ranking
+          for (const rankType of ['rating', 'review', 'nearme']) {
+            const rankPath = `${lang.id === 'en' ? '/' : `/${lang.id}/`}area/${area.areaId}/${category.id}/${rankType}`
+            routes.push(rankPath)
+          }
+        }
+      }
+    }
+    return routes
+  },
 } satisfies Config
